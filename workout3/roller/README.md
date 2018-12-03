53e43de8468d558004e9a474452015f4089504d0
richardjmin <richardjmin1997@gmai.com>
2018-12-02 23:38:17 -0800

    please

---
+++ workout3/roller/README.md
@@ -0,0 +1,7 @@
+The purpose of this assignment is to create an R package that implements functions for
+simulating rolling an object (e.g. a coin or a die) multiple times.
+
+The goal is to program two classes of objects: the "device" to be rolled, and the object
+"rolls" (containing the rolls of the "device").
+
+
--- workout3/roller/devtools-roller.R
+++ workout3/roller/devtools-roller.R
@@ -1,11 +1,14 @@
 # =====================================================
 # Devtools workflow
-#library(devtools)
+install.packages("devtools")
+library(devtools)
 # =====================================================
 
+devtools::install_github("richardjmin/roller", build_vignettes = TRUE)
 devtools::document()          # generate documentation
 devtools::check_man()         # check documentation
 devtools::test()              # run tests
 devtools::build_vignettes()   # build vignettes
 devtools::build()             # build bundle
 devtools::install()           # install package
+
